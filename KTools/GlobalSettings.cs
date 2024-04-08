5fb7bc66e30bac57492eb34bf9039534a0784421
TOFITOP\cflou <cfloutier@gmail.com>
2024-04-08 21:48:02 +0200

    added files from k2d2

---
+++ KTools/GlobalSettings.cs
@@ -0,0 +1,18 @@
+
+
+using UnityEngine;
+
+using KTools;
+
+namespace K2D2
+{
+    public class GlobalSetting
+    {
+        public static Setting<bool> settings_visible = new Setting<bool>("", false);
+    }
+}
+
+
+
+
+
